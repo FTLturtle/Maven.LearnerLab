8e5895ea858b89ba48acf2c7ecab21a7f5c9f08a
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-06 13:24:02 -0500

    added simulation interface, and changed package structure

---
+++ src/test/java/io/zipcoder/interfaces/InstructorTest.java
@@ -0,0 +1,104 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InstructorTest {
+
+    @Test
+    public void implementationTest() {
+        // Given
+        Instructor instructor = new Instructor(0);
+
+        // Then
+        Assert.assertTrue(instructor instanceof Teacher);
+    }
+
+    @Test
+    public void inheritanceTest() {
+        // Given
+        Instructor instructor = new Instructor(0);
+
+        // Then
+        Assert.assertTrue(instructor instanceof Person);
+    }
+
+    @Test
+    public void teachTest1() {
+        // Given
+        Instructor instructor = new Instructor(1);
+        Student student = new Student(0);
+        double expectedTotalStudyTime = 10.0;
+
+        // When
+        instructor.teach(student, expectedTotalStudyTime);
+        double actualTotalStudyTime = student.getTotalStudyTime();
+
+        // Then
+        Assert.assertEquals(expectedTotalStudyTime, actualTotalStudyTime, .000001);
+    }
+
+    @Test
+    public void teachTest2() {
+        // Given
+        Instructor instructor = new Instructor(1);
+        Student student = new Student(0);
+        double expectedTotalStudyTime = 19.35;
+
+        // When
+        instructor.teach(student, expectedTotalStudyTime);
+        double actualTotalStudyTime = student.getTotalStudyTime();
+
+        // Then
+        Assert.assertEquals(expectedTotalStudyTime, actualTotalStudyTime, .000001);
+    }
+
+    @Test
+    public void lectureTest1() {
+        // Given
+        Instructor instructor = new Instructor(1);
+        Student[] students = {new Student(0)};
+        double expectedTotalStudyTime = 10.0;
+
+        // When
+        instructor.lecture(students, expectedTotalStudyTime);
+        double actualTotalStudyTime = students[0].getTotalStudyTime();
+
+        // Then
+        Assert.assertEquals(expectedTotalStudyTime, actualTotalStudyTime, .000001);
+    }
+
+    @Test
+    public void lectureTest2() {
+        // Given
+        Instructor instructor = new Instructor(1);
+        Student[] students = {new Student(0), new Student(2)};
+        double teachTime = 20.0;
+        double expectedTotalStudyTime = teachTime / students.length;
+
+        // When
+        instructor.lecture(students, teachTime);
+        double actualTotalStudyTime = students[0].getTotalStudyTime();
+
+        // Then
+        Assert.assertEquals(expectedTotalStudyTime, actualTotalStudyTime, .000001);
+    }
+
+    @Test
+    public void lectureTest3() {
+        // Given
+        Instructor instructor = new Instructor(0);
+        Student[] students = {new Student(1), new Student(2), new Student(3)};
+        double teachTime = 25.0;
+        double expectedTotalStudyTime = teachTime / students.length;
+
+        // When
+        instructor.lecture(students, teachTime);
+        double actualTotalStudyTime = students[0].getTotalStudyTime();
+
+        // Then
+        Assert.assertEquals(expectedTotalStudyTime, actualTotalStudyTime, .000001);
+    }
+}
